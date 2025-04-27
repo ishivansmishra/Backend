d758db1582bad8c61cd861b0c70f4cd39bf5bfbb
ishivanshmishra <shivanshmishra2701@gmail.com>
2025-04-27 20:18:55 +0530

    utlities add

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000)
+    console.log(`Server is running at port : ${process.env.PORT}`);
+    
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed !!!",err);
+})
+
+
+
+
+
+
+
+// (async () => {
+//     try {
+//        await  mongoose.connect(`${process.env.MONGODB_URL}/
+//             ${DB_NAME}`)
+        
+//     } catch (error) {
+//         console.error("ERROR: ",error)
+//         throw err
+        
+//     }
+// })()
+
+
